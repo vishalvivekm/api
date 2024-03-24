8bda8403dc07bebbb0e4f6930017836b3ba36303
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-24 23:53:30 +0530

    query single product {id}
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- model.go
+++ model.go
@@ -1,6 +1,9 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type product struct {
 	ID       int     `json:"id"`
@@ -25,4 +28,13 @@ func getProductsFromDB(db *sql.DB) ([]product, error) {
 		products = append(products, p)
 	}
 	return products, nil
+}
+func (p *product) getProduct(db *sql.DB) error {
+	query := fmt.Sprintf("SELECT name, quantity, price FROM products where id=%v", p.ID)
+	row := db.QueryRow(query)
+	err := row.Scan(&p.Name, &p.Quantity, &p.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
