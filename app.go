dadeb296de73bba05387c5c4a4082a6ed1efa4fb
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-28 03:33:20 +0530

    handle update requests
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- app.go
+++ app.go
@@ -35,6 +35,7 @@ func (app *App) handleRoutes() {
 	app.Router.HandleFunc("/products", app.getProducts).Methods("GET")
 	app.Router.HandleFunc("/product/{id}", app.getProduct).Methods("GET")
 	app.Router.HandleFunc("/product", app.createProduct).Methods("POST")
+	app.Router.HandleFunc("/product/{id}", app.updateProduct).Methods("PUT")
 }
 
 func (app *App) getProducts(writer http.ResponseWriter, request *http.Request) {
@@ -81,6 +82,27 @@ func (app *App) createProduct(writer http.ResponseWriter, request *http.Request)
 	sendResponse(writer, http.StatusOK, p)
 
 }
+func (app *App) updateProduct(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		sendError(writer, http.StatusBadRequest, "invalid product ID")
+		return
+	}
+	var p product
+	err = json.NewDecoder(request.Body).Decode(&p)
+	if err != nil {
+		sendError(writer, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	p.ID = key
+	err = p.updateProductInDB(app.DB)
+	if err != nil {
+		sendError(writer, http.StatusInternalServerError, fmt.Sprintf("error while updating the product in db, err: %v", err.Error()))
+		return
+	}
+	sendResponse(writer, http.StatusOK, p)
+}
 func sendResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("content-type", "application/json")
@@ -94,3 +116,51 @@ func sendError(w http.ResponseWriter, statusCode int, err string) {
 	errorMsg := map[string]string{"error: ": err}
 	sendResponse(w, statusCode, errorMsg)
 }
+
+/*//type jsonResponse struct {
+//	Msg           string `json:"msg"`
+//	UpdatedProduct string `json:"update product"`
+//}
+
+func (app *App) updateProduct(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		sendError(writer, http.StatusBadRequest, "invalid product ID")
+		return
+	}
+
+	var p product
+	err = json.NewDecoder(request.Body).Decode(&p)
+	if err != nil {
+		sendError(writer, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	p.ID = key
+	err = p.updateProductInDB(app.DB)
+	if err != nil {
+		sendError(writer, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	//msg := jsonResponse{
+	//	Msg:            "success",
+	//	UpdatedProduct: fmt.Sprintf("updatedProduct: %v", p),
+	//}
+
+	//sendResponse(writer, http.StatusOK, msg)
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("could not marshal response, err: %v", err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_, err = w.Write(response)
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, fmt.Sprintf("could not write response, err: %v", err.Error()))
+	}
+}*/
