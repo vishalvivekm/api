c17b7cfb97ea18c1b802b912c817406fd7a2f548
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-28 18:19:26 +0530

    remove whitespace from json tag causing failing test
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- model.go
+++ model.go
@@ -9,7 +9,7 @@ import (
 
 type product struct {
 	ID       int     `json:"id"`
-	Name     string  `json: "name"`
+	Name     string  `json:"name"`
 	Quantity int     `json:"quantity"`
 	Price    float64 `json:"price"`
 }
