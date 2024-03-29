98a7f1fcb9b57e223f10a13d3d6cb4a48457ca49
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-29 23:55:07 +0530

    add test for update route
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- app_test.go
+++ app_test.go
@@ -100,3 +100,81 @@ func TestDeleteProduct(t *testing.T) {
 	response = sendRequest(req)
 	checkStatusCode(t, http.StatusNotFound, response.Code)
 }
+
+func TestUpdateProduct(t *testing.T) {
+	clearTable()
+	addProduct("connector", 10, 10)
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := sendRequest(req)
+	checkStatusCode(t, http.StatusOK, response.Code)
+
+	var initialGetResponseValue map[string]interface{}
+
+	err := json.Unmarshal(response.Body.Bytes(), &initialGetResponseValue)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	putreqBody := []byte(`{"name": "pens", "quantity": 5, "price": 20.00}`)
+	//reqMap := map[string]interface{}{"name": "pens", "quantity": 5, "price": 20.00}
+	//putreqBody, _ := json.Marshal(reqMap)
+	req, _ = http.NewRequest("PUT", "/product/1", bytes.NewBuffer(putreqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	response = sendRequest(req)
+	checkStatusCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = sendRequest(req)
+	checkStatusCode(t, http.StatusOK, response.Code)
+
+	var getResponseValueAfterPutReq map[string]interface{}
+
+	err = json.Unmarshal(response.Body.Bytes(), &getResponseValueAfterPutReq)
+	if err != nil {
+		t.Errorf("can not unmarshal, %v", err.Error())
+	}
+	log.Println(initialGetResponseValue)
+	log.Println(getResponseValueAfterPutReq)
+	/*
+	   2024/03/29 23:54:02 map[id:1 name:connector price:10 quantity:10]
+	   2024/03/29 23:54:02 map[id:1 name:pens price:20 quantity:5]
+
+	*/
+	if initialGetResponseValue["id"] != getResponseValueAfterPutReq["id"] {
+		t.Errorf("expected id: %v, got: %v", initialGetResponseValue["id"], getResponseValueAfterPutReq["id"])
+	}
+	if initialGetResponseValue["name"] == getResponseValueAfterPutReq["name"] {
+		t.Errorf("expected name: %v, got: %v", getResponseValueAfterPutReq["name"], initialGetResponseValue["name"])
+	}
+	if initialGetResponseValue["quantity"] == getResponseValueAfterPutReq["quantity"] {
+		t.Errorf("expected quantity: %v, got: %v", getResponseValueAfterPutReq["quantity"], initialGetResponseValue["quantity"])
+	}
+	if initialGetResponseValue["price"] == getResponseValueAfterPutReq["price"] {
+		t.Errorf("expected price: %v, got: %v", getResponseValueAfterPutReq["quantity"], initialGetResponseValue["price"])
+	}
+}
+
+//func TestUpdateProduct(t *testing.T) {
+//	clearTable()
+//	addProduct("connector", 10, 10)
+//
+//	req, _ := http.NewRequest("GET", "/product/1", nil)
+//	response := sendRequest(req)
+//	checkStatusCode(t, http.StatusOK, response.Code)
+//	//log.Printf("inside updatePRoductTEst: %v", string(response.Body.Bytes())) //  inside updatePRoductTEst: {"id":1,"name":"connector","quantity":10,"price":10}
+//	q := map[string]interface{}{"name": "pens", "quantity": 5, "price": 20.00}
+//	bf, err := json.Marshal(q)
+//	if err != nil {
+//		log.Fatalf("can't marshal q, %v", err)
+//	}
+//	req, _ = http.NewRequest("PUT", "/product/1", bytes.NewBuffer(bf))
+//	response = sendRequest(req)
+//	checkStatusCode(t, http.StatusOK, response.Code)
+//	//log.Printf("inside updatePRoductTEst: %v", string(response.Body.Bytes())) // inside updatePRoductTEst: {"id":1,"name":"pens","quantity":5,"price":20}
+//
+//	req, _ = http.NewRequest("GET", "/product/1", nil)
+//	response = sendRequest(req)
+//	checkStatusCode(t, http.StatusOK, response.Code)
+//
+//}
